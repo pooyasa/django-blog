f22a91472c7956d0ec3ce7857b37d0cd7603f323
Pooya Sajjadi <pooyasajjadi@gmail.com>
2023-04-17 10:32:59 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -29,6 +29,7 @@ from blango_auth.forms import BlangoRegistrationForm
 urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),    
     path('admin/', admin.site.urls),
     path('ip/', blog.views.get_ip),
     path(
